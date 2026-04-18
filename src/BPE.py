f09f1fd2a6de7a16d8e5a7c1ca9627654b51b196
Xenow91 <link.maherault@gmail.com>
2026-04-19 00:15:45 +0200

    Final python version
    le code fonctionne mais est beaucoup trop lent avec un vocabulaire cible de 32000 je n'avais pas les résultats au bout de 5h30. Il faut ajouter une optimisation sur le calcul des pairs en utilisant un tas-max et en modifiant uniquement le compteur au byte voisin XABY si AB est ma paire

--- src/BPE.py
+++ src/BPE.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import pickle
 from collections import Counter
 
 df_parquet=pd.read_parquet('C:/Users/linkm/Documents/GitHub/IA/data/raw/fineweb.parquet', columns=['text'])
@@ -23,7 +24,7 @@ for article in processed_dataset:
 
 
 vocab={(i,):i for i in range(256)}
-inverse_vocab=[i for i in range(256)]
+inverse_vocab=[(i,) for i in range(256)]
 
 
 def count():
@@ -35,7 +36,12 @@ def count():
 
 
 def merge(id1, id2, new_id):
+    new_word_freqs=Counter()
     for word in word_freqs:
+        if id1 not in word:
+            new_word_freqs[word] = word_freqs[word]
+            continue
+
         k=0
         new_word=[]
         while k<len(word):
@@ -43,17 +49,22 @@ def merge(id1, id2, new_id):
                 new_word.append(word[k])
                 k+=1
                 continue
-            if word[k+1]==id2 :
+            if (k+1)<len(word) and word[k+1]==id2 :
                 new_word.append(new_id)
                 k+=2
+                continue
+            new_word.append(word[k])
+            k+=1
+
         new_word=tuple(new_word)
-        if word!=new_word:
-            word_freqs[new_word]=word_freqs[word]
-            del word_freqs[word]
+        
+        new_word_freqs[new_word]=word_freqs[word]
+    
+    return new_word_freqs
 
 
 taille=256
-while taille<32000 :
+while taille<100 :
     pairs=count()
 
     max=pairs.most_common(1)
@@ -62,6 +73,11 @@ while taille<32000 :
     vocab[(byte1,byte2)]=taille
     inverse_vocab.append((byte1,byte2))
 
-    merge(byte1,byte2,taille)
+    word_freqs=merge(byte1,byte2,taille)
 
     taille+=1
+
+with open ('C:/Users/linkm/Documents/GitHub/IA/data/processed/vocab.pkl', 'wb') as f:
+    pickle.dump(vocab,f)
+with open ('C:/Users/linkm/Documents/GitHub/IA/data/processed/inverse_vocab.pkl', 'wb') as f:
+    pickle.dump(inverse_vocab,f)
